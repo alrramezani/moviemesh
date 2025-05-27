0ff68aac348c7182400ad5bcda08dcc70d0cf47c
alr.ramezani <alr.ramezani@gmail.com>
2025-05-27 21:17:19 +0200

    first deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/moviemesh', // name of your GitHub repo
+  images: {
+    unoptimized: true, // disables next/image optimization for static export
+  },
 };
 
 export default nextConfig;
