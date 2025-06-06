595d32fea2be6f7b2025965822588d2d23f3305a
alr.ramezani <alr.ramezani@gmail.com>
2025-06-06 18:22:57 +0200

    add favicon

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/moviemesh", // name of your GitHub repo
   images: {
-    unoptimized: true, // disables next/image optimization for static export
+    // unoptimized: true, // disables next/image optimization for static export
     remotePatterns: [
       {
         protocol: "https",
