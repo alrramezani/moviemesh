90d3a53bacd3c790ce7e480d5e7e788b1863bed5
alr.ramezani <alr.ramezani@gmail.com>
2025-06-06 18:13:28 +0200

    new nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // basePath: '/moviemesh', // name of your GitHub repo
-  // images: {
-  //   unoptimized: true, // disables next/image optimization for static export
-  // },
+  output: "export",
+  basePath: "/moviemesh", // name of your GitHub repo
   images: {
+    unoptimized: true, // disables next/image optimization for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'image.tmdb.org', // replace with the domain of your images
-        pathname: '/**',
+        protocol: "https",
+        hostname: "image.tmdb.org", 
+        pathname: "/**",
       },
     ],
   },
